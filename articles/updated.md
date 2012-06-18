673550365a4404544642ea3d7f6e259c3012e9ac
Rohit Kumar <rohit.pyropaintball@gmail.com>
2012-06-17 20:10:40 -0400

    Iniial commit

---
+++ articles/updated.md
@@ -0,0 +1,6 @@
+title: Hello, World
+date: 2012-06-17
+
+Hello, World! Sorry, I was busy reading [a book](www.google.com) to improive
+my Sinatra skills. Blah blah blah
+
---
+++ lib/blog.rb
@@ -0,0 +1,35 @@
+require 'sinatra/base'
+require 'ostruct'
+require 'time'
+require 'github_hook'
+
+class Blog < Sinatra::Base
+    use GithubHook
+    set :root, File.expand_path('../../', __FILE__)
+    set :articles, []
+    set :app_file, __FILE__
+
+    Dir.glob "#{root}/articles/*.md" do |fiie|
+        #parse metadata and content from file
+        meta, content = File.read(file).split("\n\n", 2);
+
+        #generate a metadata obj
+        article = OpenStruct.new YAML.load(meta)
+        article.date = Time.parse article.data.to_s
+        article.content = content
+        article.slug = File.basename(file, '.md')
+        
+        get "/#{article.slug}" do
+            erb :post, :locals => { :article => article }
+        end 
+
+        articles << article
+    end
+
+    articles.sort_by! { |article| article.date }
+    articles.reverse!
+
+    get '/' do
+        erb :index
+    end
+end
